7306ee876039a1677934c9d77ad4bdd864ff46a6
roney ruiz <roneyruizrojas@gmail.com>
2024-11-16 10:39:02 -0600

    feat: shadowing, mutability and compound types

--- crates/libs/lib-variables/src/lib.rs
+++ crates/libs/lib-variables/src/lib.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, vec};
+
 pub fn show_variable_types() {
     let integer: i32 = 100;
     println!("The value of 'integer' is: {}", integer);
@@ -12,10 +14,47 @@ pub fn show_variable_types() {
     println!("The value of 'stringer' is: {}", stringer);
 }
 
+
 pub fn show_compound_types() {
     let tuple: (i32, bool) = (100, false);
     println!("The value of the tuple is: {:?}", tuple);
 
     let array: [&str; 5] = ["a", "b", "c", "d", "e"];
     println!("The value of the array is: {:?}", array);
+
+    let mut vector: Vec<i32> = vec![1, 2, 3, 4];
+    vector.push(100);
+    println!("The value of the vector is: {:?}", vector);
+
+    let hashmap: HashMap<&str, i32> = HashMap::from([
+        ("first", 12), ("second", 24)
+    ]);
+    println!("The value of the hashmap is: {:?}", hashmap);
+}
+
+
+pub fn mutability_example() {
+    let mut text: String = String::from("first text");
+    println!("Mutability first value: {}", text);
+
+    text = String::from("second text");
+    println!("Mutability second value: {}", text);
+}
+
+
+pub fn shadowing_example() {
+    let exe: String = String::from("abc");
+
+    let exe: usize = exe.len();
+
+    println!("Shadow value: {}", exe);
+}
+
+
+pub fn variables_facade() {
+    show_variable_types();
+    show_compound_types();
+
+    mutability_example();
+    shadowing_example();
 }
--- crates/mainner/src/main.rs
+++ crates/mainner/src/main.rs
@@ -1,6 +1,5 @@
 use lib_variables::*;
 
 fn main() {
-    show_variable_types();
-    show_compound_types();
+    variables_facade();
 }
