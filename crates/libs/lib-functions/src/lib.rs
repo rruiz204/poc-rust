a6e3d2c9f1782f83b2e5c53ca90699f03b0f1b74
roney ruiz <roneyruizrojas@gmail.com>
2024-11-17 22:59:40 -0600

    feat: learn more about functions

--- crates/libs/lib-functions/src/lib.rs
+++ crates/libs/lib-functions/src/lib.rs
@@ -1,8 +1,20 @@
-fn function_with_parameters(a: i32, b: i32) -> i32 {
-    a * b
+fn with_parameters_example(number: i32) {
+    println!("{}", number);
 }
 
+fn with_returns(a: i32, b: i32) -> i32 {
+    a + b
+}
 
 pub fn functions_facade() {
-    println!("The result is: {}", function_with_parameters(3, 6));
+    println!("=== Functions with Parameters ===");
+    with_parameters_example(123);
+
+    println!("=== Functions with Returns ===");
+    let result: i32 = with_returns(10, 12);
+    println!("{}", result);
+
+    println!("=== Clousures Functions ===");
+    let multiply = |x: i32, y:i32| println!("{}", x * y);
+    multiply(5, 10);
 }
