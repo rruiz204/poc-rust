f2cca685f8f70e71aab05824c65e15b19512ce36
roney ruiz <roneyruizrojas@gmail.com>
2024-11-24 20:32:03 -0600

    chore: some refactors

--- crates/libs/lib-variables/src/lib.rs
+++ crates/libs/lib-variables/src/lib.rs
@@ -1,38 +1,3 @@
-use std::{collections::HashMap, vec};
-
-fn show_variable_types() {
-    let integer: i32 = 100;
-    println!("The value of 'integer' is: {}", integer);
-
-    let floatter: f64 = 3.4324;
-    println!("The value of 'floatter' is: {}", floatter);
-
-    let maybe: bool = true;
-    println!("The value of 'maybe' is: {}", maybe);
-
-    let stringer: String = String::from("idk man :b");
-    println!("The value of 'stringer' is: {}", stringer);
-}
-
-
-fn show_compound_types() {
-    let tuple: (i32, bool) = (100, false);
-    println!("The value of the tuple is: {:?}", tuple);
-
-    let array: [&str; 5] = ["a", "b", "c", "d", "e"];
-    println!("The value of the array is: {:?}", array);
-
-    let mut vector: Vec<i32> = vec![1, 2, 3, 4];
-    vector.push(100);
-    println!("The value of the vector is: {:?}", vector);
-
-    let hashmap: HashMap<&str, i32> = HashMap::from([
-        ("first", 12), ("second", 24)
-    ]);
-    println!("The value of the hashmap is: {:?}", hashmap);
-}
-
-
 fn mutability_example() {
     let mut text: String = String::from("first text");
     println!("Mutability first value: {}", text);
@@ -52,9 +17,9 @@ fn shadowing_example() {
 
 
 pub fn variables_facade() {
-    show_variable_types();
-    show_compound_types();
-
+    println!("=== Mutability/Immutability ===");
     mutability_example();
+
+    println!("=== Shadowing ===");
     shadowing_example();
 }
--- crates/mainner/src/main.rs
+++ crates/mainner/src/main.rs
@@ -1,7 +1,9 @@
 use lib_ownership::ownership_facade;
 use lib_functions::functions_facade;
+use lib_variables::variables_facade;
 
 fn main() {
+    variables_facade();
     functions_facade();
     ownership_facade();
 }
