675711f262af9713242651e9468bc766e9cd1a52
roney ruiz <roneyruizrojas@gmail.com>
2024-11-24 21:12:33 -0600

    feat: learn about structures and methods

--- crates/mainner/src/main.rs
+++ crates/mainner/src/main.rs
@@ -1,9 +1,11 @@
 use lib_ownership::ownership_facade;
 use lib_functions::functions_facade;
 use lib_variables::variables_facade;
+use lib_structures::structures_facade;
 
 fn main() {
     variables_facade();
     functions_facade();
     ownership_facade();
+    structures_facade();
 }
