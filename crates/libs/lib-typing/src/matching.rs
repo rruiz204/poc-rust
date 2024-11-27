10aa47a8d30bbe7be68f1b8d88143f47d95949a8
roney ruiz <roneyruizrojas@gmail.com>
2024-11-27 14:42:01 -0600

    reinforce: pattern matching

--- crates/libs/lib-typing/src/matching.rs
+++ crates/libs/lib-typing/src/matching.rs
@@ -1,13 +1,26 @@
-fn rolling(number: &i32) {
-    match number {
-        1 => println!("One"),
-        2 => println!("Two"),
-        _ => println!("Other")
-    }
+struct User {
+  id: i32,
+  user: String,
+}
+
+fn get_user(user_id: i32, users: Vec<User>) -> Option<User> {      
+  users.into_iter().find(|user| user.id == user_id)
 }
 
 pub fn matching_showcase() {
   println!("=== Matching ===");
-  let number: i32 = 2;
-  rolling(&number);
+  
+  let users: Vec<User> = vec![
+    User { id: 1, user: String::from("user1") },
+    User { id: 2, user: String::from("user2") },
+    User { id: 3, user: String::from("user3") },
+  ];
+
+  let user = get_user(4, users);
+
+  if let Some(value) = user {
+    println!("ID: {} User: {}", value.id, value.user);
+  } else {
+    println!("User not found");
+  }
 }
