ec743263d98f2584db4d5ae23229f24c15c910d9
roney ruiz <roneyruizrojas@gmail.com>
2024-11-28 11:38:32 -0600

    know: learn about structs and traits

--- crates/libs/lib-structures/src/lib.rs
+++ crates/libs/lib-structures/src/lib.rs
@@ -1,42 +1,11 @@
-#[derive(Debug)]
-struct User {
-    id: i32,
-    username: String,
-    email: String,
-}
+mod structs;
+mod traits;
 
-impl User {
-    fn get_email(&self) -> &String {
-        &self.email
-    }
-}
-
-impl User {
-    fn get_username(&self) -> &String {
-        &self.username
-    }
-}
-
-impl User {
-    fn get_id(&self) -> &i32 {
-        &self.id
-    }
-}
+use crate::structs::structs_showcase;
+use crate::traits::traits_showcase;
 
 pub fn structures_facade() {
     println!("=== Structures ===");
-
-    let change_email = |user: &mut User, email: String| user.email = email;
-
-    let mut pato = User {
-        id: 1,
-        username: String::from("pato"),
-        email: String::from("pato@example.com"),
-    };
-
-    change_email(&mut pato, String::from("pato213@example.com"));
-
-    println!("The ID is: {}", pato.get_id());
-    println!("The Username is: {}", pato.get_username());
-    println!("The Email is: {}", pato.get_email());
+    structs_showcase();
+    traits_showcase();
 }
